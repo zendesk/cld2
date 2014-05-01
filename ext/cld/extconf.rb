4fd79713fd0374816b4859fc49e8977c5de3a0ad
Alessandro Dal Grande <alessandro@teambanjo.com>
2014-04-30 18:00:19 -0700

    more robust extconf

--- ext/cld/extconf.rb
+++ ext/cld/extconf.rb
@@ -1,1 +1,7 @@
-system "cmake . -DCMAKE_BUILD_TYPE=Release" unless File.exists?('Makefile')
+require 'mkmf'
+
+if !find_executable('cmake')
+  abort "ERROR: CMake is required to build CLD."
+end
+
+`cmake -DCMAKE_BUILD_TYPE=Release .` unless File.exists?('Makefile')
