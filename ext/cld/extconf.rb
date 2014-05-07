3b438b29c21e26b9219d24561a567b6535e5579f
Alessandro Dal Grande <alessandro@teambanjo.com>
2014-05-06 18:14:26 -0700

    link to standard C++ library

--- ext/cld/extconf.rb
+++ ext/cld/extconf.rb
@@ -27,7 +27,9 @@ $objs = ["internal/cldutil.o",
   "internal/cld_generated_score_quad_octa_0122_2.o",
   "thunk.o"]
 
-create_makefile('libcld2')
+if have_library('stdc++')
+  create_makefile('libcld2')
+end
 
 # to clean up object files under internal subdirectory.
 open('Makefile', 'a') do |f|
